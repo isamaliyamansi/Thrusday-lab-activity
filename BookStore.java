9f5cb6018ea9076b7555cbf92c7134fc7abe5751
isamaliyamansi <109841693+isamaliyamansi@users.noreply.github.com>
2022-09-08 21:01:26 +0530

    Add files via upload

---
+++ BookStore.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+class Book
+{
+	int BookID ;
+	String title ;
+	String author ;
+	String category ;
+	float price ;
+	public Book(int BookID,String title,String author,String category,float price)
+	{
+		super();
+		this.id=id;
+		this.title=title;
+		this.author=author;
+		this.category=category;
+		this.price;
+	}
+	}
+	public class BookStore
+	{
+		public static void main(String args[])
+		{
+		list<addBook>=new Arraylist<addBook>();
+		list.add(new addBook(1,"java","E balaguruswamy","coding",560);
+        list.add(new addBook(2,"C++","E balaguruswamy","coding",450);
+        list.add(new addBook(3,"python","james","coding",340);
+        list.add(new addBook(4,"C","E balaguruswamy","coding",460);
+		List<Integer>squareNumbers=numbers.stream().collect(Collectors.toList());
+		System.out.println("Contents of the array list:");
+		for(String element:list){
+			if(element.contains("java")){
+				System.out.println(element);
+			}
+			if(element.contains("C++")){
+				System.out.println(element);
+			}
+			if(element.contains("python")){
+				System.out.println(element);
+		   }
+		   if(element.contains("C")){
+				System.out.println(element);		
+	}
+	
+	list<String>searchByTitle=mylist.stream().map(b-> b.name(Collectors.tolist());
+	System.out.println(searchByTitle);
+	
+	
+	list<String>earchByAuthor =mylist.stream().map(p1 ->p1.author).collect((Collectors.tolist());
+	System.out.println(searchByAuthor);
+	
+	
+	
+}
